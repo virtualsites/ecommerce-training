633ec9003b78bc5657e2d2a879fac3d2716db7b6
bozenapietrzak <bozena@landingi.com>
2019-02-11 23:10:02 +0100

    item entity spec test

---
+++ spec/Domain/ItemSpec.php
@@ -0,0 +1,19 @@
+<?php
+namespace spec\Domain;
+
+use PhpSpec\ObjectBehavior;
+
+class ItemSpec extends ObjectBehavior
+{
+    public function it_is_return_price()
+    {
+        $this->beConstructedWith('Item', 15.6);
+        $this->getPrice()->shouldReturn(15.60);
+    }
+
+    public function it_is_return_price_as_double_type()
+    {
+        $this->beConstructedWith('Item', 4);
+        $this->getPrice()->shouldBeDouble();
+    }
+}
